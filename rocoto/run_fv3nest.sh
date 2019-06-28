978259a989be8fe770738e5e469cfd3dd05734c0
matthew.pyle <Matthew.Pyle@noaa.gov>
2019-06-28 16:48:19 +0000

    Changes all fv3cam_workflow instances to regional_workflow

--- rocoto/run_fv3nest.sh
+++ rocoto/run_fv3nest.sh
@@ -9,4 +9,4 @@ module load lsf/10.1
 module use /gpfs/dell3/usrx/local/dev/emc_rocoto/modulefiles
 module load ruby/2.5.1 rocoto/1.2.4
 
-rocotorun -v 10 -w /gpfs/dell2/emc/modeling/noscrub/${USER}/fv3cam_workflow/rocoto/drive_fv3nest.xml -d /gpfs/dell2/emc/modeling/noscrub/${USER}/fv3cam_workflow/rocoto/drive_fv3nest.db
+rocotorun -v 10 -w /gpfs/dell2/emc/modeling/noscrub/${USER}/regional_workflow/rocoto/drive_fv3nest.xml -d /gpfs/dell2/emc/modeling/noscrub/${USER}/regional_workflow/rocoto/drive_fv3nest.db
