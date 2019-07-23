ca52750295ae9d69b1037064e2c66f3ff4ecd52b
Bin Liu <bin.liu@noaa.gov>
2019-07-23 01:58:36 +0000

    feature/buildregionalgrid: Import the regional_grid into the build system.

--- sorc/build_utils.sh
+++ sorc/build_utils.sh
@@ -62,6 +62,14 @@ echo " .... Building sfc_climo_gen .... "
 ./build_sfc_climo_gen.sh > $logs_dir/build_sfc_climo_gen.log 2>&1
 }
 
+#------------------------------------
+# build regional_grid
+#------------------------------------
+$Build_regional_grid && {
+echo " .... Building regional_grid .... "
+./build_regional_grid.sh > $logs_dir/build_regional_grid.log 2>&1
+}
+
 cd $build_dir
 
 echo 'Building utils done'
