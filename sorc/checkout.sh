c5eac5dfa1eab377474d58a24362b558b7a0c1a5
Bin Liu <Bin.Liu@noaa.gov>
2019-08-12 01:32:28 +0000

    feature/merge_comm_to_EMC: Update the checkout.sh script to be able to pull the latest updates from the corresponding components' branches.

--- sorc/checkout.sh
+++ sorc/checkout.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-set -xu
+set -xue
 
-echo "INFO: Checking out the regional_workflow external components."
-echo "INFO: If the directory of an external component already exist, this checkout.sh script will do nothing for that component."
+echo "INFO: Clone and Check out the regional_workflow external components."
+echo "INFO: If the directory of an external component already exist, will pull the latest updates from the corresponding branch."
 
 if [[ $# -ge 1 ]]; then
   if [[ $1 = "-c" ]]; then
@@ -28,53 +28,62 @@ echo $topdir
 
 echo NEMSfv3gfs checkout ...
 if [[ ! -d regional_forecast.fd ]] ; then
-    rm -f ${topdir}/checkout-forecast.log
-    git clone --recursive gerrit:NEMSfv3gfs regional_forecast.fd >> ${topdir}/checkout-forecast.log 2>&1
+    git clone -b regional --recursive gerrit:NEMSfv3gfs regional_forecast.fd
+    #cd regional_forecast.fd
+    #git checkout regional
+    #git submodule update --init --recursive
+    #cd ${topdir}
+else
+    echo 'Directory regional_forecast.fd already exists. Pull the latest updates from the corresponding branch.'
     cd regional_forecast.fd
-#    git checkout nemsfv3gfs_beta_v1.0.12
     git checkout regional
+    git pull
     git submodule update --init --recursive
     cd ${topdir}
-else
-    echo 'Skip.  Directory regional_forecast.fd already exists.'
 fi
 
 echo EMC_post checkout ...
 if [[ ! -d regional_post.fd ]] ; then
-    rm -f ${topdir}/checkout-post.log
-    git clone --recursive gerrit:EMC_post regional_post.fd >> ${topdir}/checkout-post.log 2>&1
-    cd regional_post.fd
-#    git checkout ncep_post.v8.0.27
-    git checkout regional
-    cd ${topdir}
+    git clone -b regional  --recursive gerrit:EMC_post regional_post.fd
+    #cd regional_post.fd
+    #git checkout regional
+    #cd ${topdir}
 else
-    echo 'Skip.  Directory regional_post.fd already exists.'
+    echo 'Directory regional_post.fd already exists. Pull the latest updates from the corresponding branch.'
+    cd regional_post.fd
+    git checkout regional
+    git pull
+    cd ${topdir}
 fi
 
 echo UFS_UTILS checkout ...
 if [[ ! -d regional_utils.fd ]] ; then
-    rm -f ${topdir}/checkout-utils.log
-    git clone --recursive gerrit:UFS_UTILS regional_utils.fd >> ${topdir}/checkout-utils.log 2>&1
-	cd regional_utils.fd
-#	git checkout develop
-	git checkout feature/HAFS
-    cd ${topdir}
+    git clone -b feature/HAFS --recursive gerrit:UFS_UTILS regional_utils.fd
+	#cd regional_utils.fd
+	#git checkout feature/HAFS
+    #cd ${topdir}
 else
-    echo 'Skip.  Directory regional_utils.fd already exists.'
+    echo 'Directory regional_utils.fd already exists. Pull the latest updates from the corresponding branch.'
+	cd regional_utils.fd
+	git checkout feature/HAFS
+	git pull
+    cd ${topdir}
 fi
 
 echo ProdGSI checkout ...
 if [[ ! -d regional_gsi.fd ]] ; then
-    rm -f ${topdir}/checkout-gsi.log
-    git clone --recursive gerrit:ProdGSI regional_gsi.fd >> ${topdir}/checkout-gsi.log 2>&1
+    git clone -b regional --recursive gerrit:ProdGSI regional_gsi.fd
+    #cd regional_gsi.fd
+    #git checkout regional
+    #git submodule update --init --recursive
+    #cd ${topdir}
+else
+    echo 'Directory regional_gsi.fd already exists. Pull the latest updates from the corresponding branch.'
     cd regional_gsi.fd
-#    git checkout fv3da.v1.0.37
     git checkout regional
     git submodule update --init --recursive
+	git pull
     cd ${topdir}
-#    cp ../parm/anavinfo_fv3_64 regional_gsi.fd/fix
-else
-    echo 'Skip.  Directory regional_gsi.fd already exists.'
 fi
 
 exit
