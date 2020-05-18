393cf5a45b1f7c621ba7c144333aef6083b05048
JAAravequia <53572635+JAAravequia@users.noreply.github.com>
2020-05-18 15:53:41 -0400

    Delete my_cron.sh

--- rocoto/my_cron.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -l
-set -x
-module load rocoto/1.3.1
-export HOMEfv3=/work/noaa/fv3-cam/jaravequ/regional_workflow
-cd ${HOMEfv3}/rocoto
-rocotorun -v 10 -w fv3sartest_2020012300.xml -d fv3sartest_2020012300.db
-echo 'job done'
-
