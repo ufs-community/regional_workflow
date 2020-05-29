44d349f4aef6f17d363c72f6e9d2a50f352af53b
gerard ketefian <gerard.ketefian@noaa.gov>
2020-05-29 12:02:25 -0600

    Modify the way the temporary fix for the suite_FV3_GFS_2017_gfdlmp_regional physics suite works.
    Modify this fix so that the suite definition file (SDF) is not changed in the ufs_weather_model directory where the clone of the ufs-weather-model repo is located (so that no changes to tracked files are reported in the clone of that repo) and instead, the SDF is first copied to the experiment directory and changed there.
    
    Modifications common to more than one file (used below in listing of file-by-file modifications):
    ------------------------------------------------------------------------------------------------
    (A) Fix/add/delete comments and/or informational and/or error messages.
    
    File-by-file modifications:
    --------------------------
    
    jobs/JREGIONAL_RUN_POST: (A)
    
    ush/config_defaults.sh:
    * Remove extra spaces at ends of elements of array CYCLEDIR_LINKS_TO_FIXam_FILES_MAPPING.
    
    ush/generate_FV3SAR_wflow.sh: (A)
    * Put in an if-statement that renames the temporary SDF (suite_FV3_GFS_2017_gfdlmp_regional.xml.tmp) created in the experiment directory by the temporay fix in ush/setup.sh (see below) for the FV3_GFS_2017_gfdlmp_regional physics suite to the actual suite file (suite_FV3_GFS_2017_gfdlmp_regional.xml).  This code needs to be removed once the SDF for this physics suite is fixed in the dtc/develop branch of the NCAR fork of the ufs-weather-model repo.
    
    ush/setup.sh: (A)
    * Change the code that is a temporary fix for the fact that the SDF for the FV3_GFS_2017_gfdlmp_regional physics suite in the dtc/develop branch of the NCAR fork of the ufs-weather-model repo is currently incorrect so that it first copies the suite defintion file to the experiment directory and makes fixes to it there (instead of fixing it directly in its original location at sorc/ufs_weather_model/...).  As part of this change:
      a) Move the creation of the experiment directory (EXPTDIR) to before the temporary code.
      b) Move the original code that calls the function set_ozone_param in the else" portion of the "if" statement that runs the temporary code.
      These two changes can be undone later after the temporary code is removed.

--- ush/config_defaults.sh
+++ ush/config_defaults.sh
@@ -1017,17 +1017,17 @@ CYCLEDIR_LINKS_TO_FIXam_FILES_MAPPING=( \
 "co2historicaldata_2011.txt | fix_co2_proj/global_co2historicaldata_2011.txt" \
 "co2historicaldata_2012.txt | fix_co2_proj/global_co2historicaldata_2012.txt" \
 "co2historicaldata_2013.txt | fix_co2_proj/global_co2historicaldata_2013.txt" \
-"co2historicaldata_2014.txt | fix_co2_proj/global_co2historicaldata_2014.txt " \
-"co2historicaldata_2015.txt | fix_co2_proj/global_co2historicaldata_2015.txt " \
-"co2historicaldata_2016.txt | fix_co2_proj/global_co2historicaldata_2016.txt " \
-"co2historicaldata_2017.txt | fix_co2_proj/global_co2historicaldata_2017.txt " \
-"co2historicaldata_2018.txt | fix_co2_proj/global_co2historicaldata_2018.txt " \
-"co2historicaldata_glob.txt | global_co2historicaldata_glob.txt " \
-"co2monthlycyc.txt          | co2monthlycyc.txt " \
-"global_h2oprdlos.f77       | global_h2o_pltc.f77 " \
-"global_zorclim.1x1.grb     | global_zorclim.1x1.grb " \
-"sfc_emissivity_idx.txt     | global_sfc_emissivity_idx.txt " \
-"solarconstant_noaa_an.txt  | global_solarconstant_noaa_an.txt " \
+"co2historicaldata_2014.txt | fix_co2_proj/global_co2historicaldata_2014.txt" \
+"co2historicaldata_2015.txt | fix_co2_proj/global_co2historicaldata_2015.txt" \
+"co2historicaldata_2016.txt | fix_co2_proj/global_co2historicaldata_2016.txt" \
+"co2historicaldata_2017.txt | fix_co2_proj/global_co2historicaldata_2017.txt" \
+"co2historicaldata_2018.txt | fix_co2_proj/global_co2historicaldata_2018.txt" \
+"co2historicaldata_glob.txt | global_co2historicaldata_glob.txt" \
+"co2monthlycyc.txt          | co2monthlycyc.txt" \
+"global_h2oprdlos.f77       | global_h2o_pltc.f77" \
+"global_zorclim.1x1.grb     | global_zorclim.1x1.grb" \
+"sfc_emissivity_idx.txt     | global_sfc_emissivity_idx.txt" \
+"solarconstant_noaa_an.txt  | global_solarconstant_noaa_an.txt" \
 "global_o3prdlos.f77        | " \
 )
 #
