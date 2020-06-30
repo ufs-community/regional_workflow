144c49eb669115767a5e05a58fe198d919767a22
Linlin.Pan <Linlin.Pan@noaa.gov>
2020-06-30 22:53:51 +0000

    adding files for getting nomads data new files:          ush/NOMADS_get_extrn_mdl_files_grib.sh          ush/NOMADS_get_extrn_mdl_files_nemsio.sh

---
+++ ush/NOMADS_get_extrn_mdl_files_grib.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Command line arguments
+if [ -z "$1" -o -z "$2" ]; then
+   echo "Usage: $0 yyyymmdd hh"
+   exit
+fi
+yyyymmdd=$1 #i.e. "20191224"
+hh=$2 #i.e. "12"
+if [ "$#" -ge 3 ]; then
+   nfcst=$3
+else 
+   nfcst=6
+fi
+if [ "$#" -ge 4 ]; then
+   nfcst_int=$4
+else 
+   nfcst_int=3
+fi
+
+# Get the data (do not need to edit anything after this point!)
+yyyymm=$((yyyymmdd/100))
+#din_loc_ic=`./xmlquery DIN_LOC_IC --value`
+mkdir -p $yyyymm/$yyyymmdd
+echo "Download files to $din_loc_ic/$yyyymm/$yyyymmdd ..."
+cd $yyyymm/$yyyymmdd
+wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.$yyyymmdd/$hh/gfs.t${hh}z.pgrb2.0p25.f000
+ifcst=$nfcst_int
+while [ $ifcst -le $nfcst ] 
+do
+echo $ifcst
+  if [ $ifcst -le 99 ]; then 
+     if [ $ifcst -le 9 ]; then
+        ifcst_str="00"$ifcst
+     else
+        ifcst_str="0"$ifcst
+     fi
+  else
+        ifcst_str="$ifcst"
+ fi
+ echo $ifcst_str
+wget -c https://nomads.ncep.noaa.gov/pub/data/nccf/com/gfs/prod/gfs.$yyyymmdd/$hh/gfs.t${hh}z.pgrb2.0p25.f${ifcst_str}
+ifcst=$[$ifcst+$nfcst_int]
+done
