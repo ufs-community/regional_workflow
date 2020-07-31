70987863bd89576885ab7b6bbd0b2d43e075fc52
Laurie Carson <laurie.carson@noaa.gov>
2020-07-31 15:00:07 -0600

    add run_post wrapper

---
+++ ush/wrappers/run_post.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+export GLOBAL_VAR_DEFNS_FP="${EXPTDIR}/var_defns.sh"
+set -x
+source ${GLOBAL_VAR_DEFNS_FP}
+export CDATE=${DATE_FIRST_CYCL}${CYCL_HRS}
+export CYCLE_DIR=${EXPTDIR}/${CDATE}
+export cyc=${CYCL_HRS}
+
+num_fcst_hrs=${FCST_LEN_HRS}
+for (( i=0; i<=$((num_fcst_hrs)); i++ )); do
+  export fhr=`printf "%02i" ${i}`
+  ${JOBSDIR}/JREGIONAL_RUN_POST
+done
