4d251de38478a0976d45ab086a3a8c5c21870be5
jeff beck <jeff.beck@noaa.gov>
2020-08-15 03:56:47 +0000

    Remove alpha and kappa parameters from the regional_grid namelist

--- scripts/exregional_make_grid.sh
+++ scripts/exregional_make_grid.sh
@@ -370,8 +370,6 @@ generation executable (exec_fp):
     'dely': ${DEL_ANGLE_Y_SG},
     'lx': ${NEG_NX_OF_DOM_WITH_WIDE_HALO},
     'ly': ${NEG_NY_OF_DOM_WITH_WIDE_HALO},
-    'a': ${JPgrid_ALPHA_PARAM},
-    'k': ${JPgrid_KAPPA_PARAM},
  }
 "
 #
