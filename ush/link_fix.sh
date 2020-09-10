050bec9a55706f8031d360284d917af142fb7568
gerard ketefian <gerard.ketefian@noaa.gov>
2020-09-10 14:32:39 -0600

    Fix indentation.

--- ush/link_fix.sh
+++ ush/link_fix.sh
@@ -247,8 +247,8 @@ Creating links in the FIXLAM directory to the grid files..."
         )
     if [ "${CCPP_PHYS_SUITE}" = "FV3_RRFS_v1beta" ]; then
       fns+=( \
-    "C*${DOT_OR_USCORE}oro_data_ss.tile${TILE_RGNL}.halo${NH0}.nc" \
-    "C*${DOT_OR_USCORE}oro_data_ls.tile${TILE_RGNL}.halo${NH0}.nc" \
+      "C*${DOT_OR_USCORE}oro_data_ss.tile${TILE_RGNL}.halo${NH0}.nc" \
+      "C*${DOT_OR_USCORE}oro_data_ls.tile${TILE_RGNL}.halo${NH0}.nc" \
            )
     fi
 
