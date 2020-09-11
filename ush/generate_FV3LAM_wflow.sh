25491ed7e322bc04db68f7ac2b98cfa70711ef31
jeff beck <jeff.beck@noaa.gov>
2020-09-11 03:29:13 +0000

    Add nrows to input.nml, HALO_BLEND to config_defaults.sh, and apply HALO_BLEND user-defined value during generate step.

--- ush/generate_FV3LAM_wflow.sh
+++ ush/generate_FV3LAM_wflow.sh
@@ -657,22 +657,6 @@ Please change one or more of these parameters or provide a value for lsoil
 
 fi
 #
-# Set magnitude of stochastic ad-hoc schemes to -999.0 if they are not
-# being used. This is required at the moment, since "do_shum/sppt/skeb"
-# does not override the use of the scheme unless the magnitude is also
-# specifically set to -999.0.  If all "do_shum/sppt/skeb" are set to
-# "false," then none will run, regardless of the magnitude values. 
-#
-if [ "${DO_SHUM}" = "false" ]; then
-  SHUM_MAG=-999.0
-fi
-if [ "${DO_SKEB}" = "false" ]; then
-  SKEB_MAG=-999.0
-fi
-if [ "${DO_SPPT}" = "false" ]; then
-  SPPT_MAG=-999.0
-fi
-#
 # Create a multiline variable that consists of a yaml-compliant string
 # specifying the values that the namelist variables that are physics-
 # suite-independent need to be set to.  Below, this variable will be
@@ -687,6 +671,7 @@ settings="\
 'fv_core_nml': {
     'target_lon': ${LON_CTR},
     'target_lat': ${LAT_CTR},
+    'nrows_blend': ${HALO_BLEND},
 #
 # Question:
 # For a ESGgrid type grid, what should stretch_fac be set to?  This depends
