2b37bcb3f12eb71c101d062c1f2ba3a4974cdeab
gerard ketefian <gerard.ketefian@noaa.gov>
2020-10-27 16:39:54 -0600

    Remove several depracated grids.
    * Remove all three GSD_HAFS grids (GSD_HAVSV0.A3km, GSD_HAVSV0.A13km, and GSD_HAVSV0.A25km).
    * Remove grids EMC_CONUS_3km and EMC_CONUS_coarse since the grids CONUS_3km_GFDLgrid and CONUS_25km_GFDLgrid are very similar to these.
    * Remove WE2E tests involving the EMC_CONUS_3km and EMC_CONUS_coarse grids (names of removed tests are nco_EMC_CONUS_3km, nco_EMC_CONUS_coarse, and nco_EMC_CONUS_coarse__suite_FV3_GFS_2017_gfdlmp).
    * Modify the WE2E test nco_ensemble to use the CONUS_25km_GFDLgrid grid instead of EMC_CONUS_coarse.
    * Add new WE2E test nco_CONUS_25km_GFDLgrid to test this grid in NCO mode.  (This test is a replacement for the removed test nco_EMC_CONUS_coarse.)
    * Update the list of WE2E tests in baselines_list.txt.
    * Update the samle NCO-mode workflow configuration file config.nco.sh to:
      a) Use the CONUS_25km_GFDLgrid grid instead of EMC_CONUS_coarse.
      b) Use the FV3_GFS_v15p2 physics suite (which is one of the two supported by the release) instead of FV3_GFS_2017_gfdlmp.
      c) Remove commented-out code.
    * Reorder the remaining grids in the case-statement in set_predef_grid_params.sh and in valid_param_vals.sh.

--- ush/config.nco.sh
+++ ush/config.nco.sh
@@ -7,11 +7,12 @@ VERBOSE="TRUE"
 RUN_ENVIR="nco"
 PREEXISTING_DIR_METHOD="rename"
 
-PREDEF_GRID_NAME="EMC_CONUS_coarse"
+PREDEF_GRID_NAME="CONUS_25km_GFDLgrid"
 GRID_GEN_METHOD="GFDLgrid"
-
 QUILTING="TRUE"
-CCPP_PHYS_SUITE="FV3_GFS_2017_gfdlmp"
+
+CCPP_PHYS_SUITE="FV3_GFS_v15p2"
+
 FCST_LEN_HRS="06"
 LBC_SPEC_INTVL_HRS="6"
 
@@ -21,15 +22,6 @@ CYCL_HRS=( "18" )
 
 EXTRN_MDL_NAME_ICS="FV3GFS"
 EXTRN_MDL_NAME_LBCS="FV3GFS"
-
-#
-# In NCO mode, the following don't need to be explicitly set to "FALSE" 
-# in this configuration file because the experiment generation script
-# will do this (along with printing out an informational message).
-#
-#RUN_TASK_MAKE_GRID="FALSE"
-#RUN_TASK_MAKE_OROG="FALSE"
-#RUN_TASK_MAKE_SFC_CLIMO="FALSE"
 
 RUN="an_experiment"
 COMINgfs="/scratch1/NCEPDEV/hwrf/noscrub/hafs-input/COMGFS"     # Path to directory containing files from the external model (FV3GFS).
