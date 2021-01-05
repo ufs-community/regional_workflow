22259e70c15ecfaac57993be3f1d1c58d691261d
gerard ketefian <gerard.ketefian@noaa.gov>
2021-01-05 15:47:40 -0700

    Continue cleanup of the way the climatology file for Thompson MP is handled.
    ush/set_thompson_mp_fix_files.sh:
    * Edit comments.
    * Remove use of the workflow variable THOMPSON_MP_CLIMO_FN.  THOMPSON_MP_CLIMO_FN is being removed from the workflow becausse it is only used in one place (in this file/function), so a local variable will suffice.
    * Set (and output via a new function argument) the new variable thompson_mp_climo_fp that gets set to either a null string (if Thompson MP is not being used in the specified physics suite) or to the full path of a climatology file (Thompson_MP_MONTHLY_CLIMO.nc) that can be used to generate approximate versions of fields needed for Thompson MP to work properly if those fields are not available in the external model files for either the ICs or the LBCs.
    
    ush/setup.sh:
    * Remove the workflow variable THOMPSON_MP_CLIMO_FN.  This variable is not needed becausse it is only used in one place (in set_thompson_mp_fix_files.sh), so it can just be a local variable in that function (thompson_mp_climo_fn).
    * Add the workflow variable THOMPSON_MP_CLIMO_FP by passing it to the function set_thompson_mp_fix_files.sh and then writing it to the workflow variable defintions file.

--- ush/setup.sh
+++ ush/setup.sh
@@ -1366,19 +1366,6 @@ LOAD_MODULES_RUN_TASK_FP="$USHDIR/load_modules_run_task.sh"
 #
 #-----------------------------------------------------------------------
 #
-# Name of the climatology file containing data that can be used to generate
-# (approximate versons of) the fields that must be present in the IC and
-# LBC files in order for the Thompson microphysics parameterization to
-# work properly.  This file is used ONLY IF the necessary fields are not
-# already present in the external model files from which the ICs and/or
-# LBCs on the native grid will be generated.
-#
-#-----------------------------------------------------------------------
-#
-THOMPSON_MP_CLIMO_FN="Thompson_MP_MONTHLY_CLIMO.nc"
-#
-#-----------------------------------------------------------------------
-#
 # Define the various work subdirectories under the main work directory.
 # Each of these corresponds to a different step/substep/task in the pre-
 # processing, as follows:
@@ -2011,11 +1998,8 @@ mkdir_vrfy -p "$EXPTDIR"
 #-----------------------------------------------------------------------
 #
 set_thompson_mp_fix_files \
-  ccpp_phys_suite_fp="${CCPP_PHYS_SUITE_IN_CCPP_FP}" 
-
-
-
-
+  ccpp_phys_suite_fp="${CCPP_PHYS_SUITE_IN_CCPP_FP}" \
+  output_varname_thompson_mp_climo_fp="THOMPSON_MP_CLIMO_FP"
 #
 #-----------------------------------------------------------------------
 #
@@ -2401,7 +2385,7 @@ FV3_EXEC_FP="${FV3_EXEC_FP}"
 
 LOAD_MODULES_RUN_TASK_FP="${LOAD_MODULES_RUN_TASK_FP}"
 
-THOMPSON_MP_CLIMO_FN="${THOMPSON_MP_CLIMO_FN}"
+THOMPSON_MP_CLIMO_FP="${THOMPSON_MP_CLIMO_FP}"
 #
 #-----------------------------------------------------------------------
 #
