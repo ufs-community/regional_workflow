6cc31165b9aac81e8184b5d4122c0336d2c2d9f2
Jamie Wolff <jwolff@ucar.edu>
2021-01-22 14:42:38 +0000

    Updated scripts to remove hard coded paths and incorporate better practices and helpful usage statements

--- ush/Python/sq_job.sh
+++ ush/Python/sq_job.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
-#SBATCH -e /scratch2/BMC/det/jwolff/GST/ufs-srweather-app-gst/regional_workflow/ush/Python/plot_allvars.log
-#SBATCH --account=gsd-fv3
+
+#SBATCH --account=an_account
 #SBATCH --qos=batch
 #SBATCH --ntasks=4
 #SBATCH --time=0:20:00
 #SBATCH --job-name="plot_allvars"
+#SBATCH --out=plot_allvars.out
 
-cd /scratch2/BMC/det/jwolff/GST/ufs-srweather-app/regional_workflow/ush/Python
+# Prior to submitting the script the following environment variables
+# must be set using export or setenv
+# HOMErrfs=/path-to/ufs-srweather-app/regional_workflow
+# EXPTDIR=/path-to/expt_dirs/your_experiment
+
+cd ${HOMErrfs}/ush/Python
 set -x
 . /apps/lmod/lmod/init/sh
 
@@ -36,12 +42,31 @@ conda activate pygraf
 ############
 # Path to shape files
 ############
-#Hera: /scratch2/NCEPDEV/fv3-cam/Chan-hoo.Jeon/tools/NaturalEarth/ 
+#Hera:
+SHAPE_FILES=/scratch2/NCEPDEV/fv3-cam/Chan-hoo.Jeon/tools/NaturalEarth
 #Jet: 
-#Orion: /home/chjeon/tools/NaturalEarth/
+#Orion: 
+#SHAPE_FILES=/home/chjeon/tools/NaturalEarth
 #Gaea: 
 
-python plot_allvars.py 2019061500 6 48 6 /scratch2/BMC/det/jwolff/GST/expt_dirs/test_CONUS_25km_GFSv15p2 /scratch2/NCEPDEV/fv3-cam/Chan-hoo.Jeon/tools/NaturalEarth
-#python plot_allvars.py 2019061518 6 12 3 /scratch2/BMC/det/jwolff/GST/expt_dirs/test_CONUS_3km_GFSv15p2 /scratch2/NCEPDEV/fv3-cam/Chan-hoo.Jeon/tools/NaturalEarth
-#python plot_allvars.py 2019061518 6 12 3 /scratch2/BMC/det/jwolff/GST/expt_dirs/test_CONUS_3km_GFSv15p2 /scratch2/NCEPDEV/fv3-cam/Chan-hoo.Jeon/tools/NaturalEarth
+export GLOBAL_VAR_DEFNS_FP="${EXPTDIR}/var_defns.sh"
+source ${GLOBAL_VAR_DEFNS_FP}
+export CDATE=${DATE_FIRST_CYCL}${CYCL_HRS}
+export FCST_START=6
+export FCST_END=${FCST_LEN_HRS}
+export FCST_INC=6
 
+# Usage statement:	Make sure all the necessary modules can be imported.
+#                       Five command line arguments are needed:
+#                       1. Cycle date/time in YYYYMMDDHH format
+#                       2. Starting forecast hour in HHH format
+#                       3. Ending forecast hour in HHH format
+#                       4. Forecast hour increment
+#                       5. EXPT_DIR: Experiment directory
+#                          -Postprocessed data should be found in the directory:
+#                            EXPT_DIR/YYYYMMDDHH/postprd/
+#                       6. CARTOPY_DIR:  Base directory of cartopy shapefiles
+#                          -File structure should be:
+#                            CARTOPY_DIR/shapefiles/natural_earth/cultural/*.shp
+
+python plot_allvars.py ${CDATE} ${FCST_START} ${FCST_END} ${FCST_INC} ${EXPTDIR} ${SHAPE_FILES}
