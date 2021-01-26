df3026b2883eea70540dbe0c1a1b4b00b34c5fb8
Michael Kavulich, Jr <kavulich@ucar.edu>
2021-01-26 09:51:42 -0700

    Simplify Darwin-specific bash utility errors

--- ush/bash_utils/define_macos_utilities.sh
+++ ush/bash_utils/define_macos_utilities.sh
@@ -15,15 +15,27 @@
 #
 #-----------------------------------------------------------------------
 #
+darwinerror () {
+
+  utility=$1
+  echo >&2 "
+For Darwin-based operating systems (MacOS), the '${utility}' utility is required to run the UFS SRW Application.
+Reference the User's Guide for more information about platform requirements.
+
+Aborting.
+"
+  exit 1
+}
+
   if [[ $(uname -s) == Darwin ]]; then
     export READLINK=greadlink
-    command -v $READLINK >/dev/null 2>&1 || { echo >&2 "For Darwin-based operating systems (MacOS), the '$READLINK' utility is required to run the UFS SRW Application. Reference the User's Guide for more information about platform requirements. Aborting."; exit 1; }
+    command -v $READLINK >/dev/null 2>&1 || darwinerror $READLINK
     export SED=gsed
-    command -v $SED >/dev/null 2>&1 || { echo >&2 "For Darwin-based operating systems (MacOS), the '$SED' utility is required to run the UFS SRW Application. Reference the User's Guide for more information about platform requirements. Aborting."; exit 1; }
+    command -v $SED >/dev/null 2>&1 || darwinerror $SED
     export DATE_UTIL=gdate
-    command -v $DATE_UTIL >/dev/null 2>&1 || { echo >&2 "For Darwin-based operating systems (MacOS), the '$DATE_UTIL' utility is required to run the UFS SRW Application. Reference the User's Guide for more information about platform requirements. Aborting."; exit 1; }
+    command -v $DATE_UTIL >/dev/null 2>&1 || darwinerror $DATE_UTIL
     export LN_UTIL=gln
-    command -v $LN_UTIL >/dev/null 2>&1 || { echo >&2 "For Darwin-based operating systems (MacOS), the '$LN_UTIL' utility is required to run the UFS SRW Application. Reference the User's Guide for more information about platform requirements. Aborting."; exit 1; }
+    command -v $LN_UTIL >/dev/null 2>&1 || darwinerror $LN_UTIL
   else
     export READLINK=readlink
     export SED=sed
