7a2254998d0616dd5d17957167917ae54be9d9d0
Michael Kavulich, Jr <kavulich@ucar.edu>
2021-01-30 09:26:31 -0700

    Fix dumb mistake in launch script

--- ush/launch_FV3LAM_wflow.sh
+++ ush/launch_FV3LAM_wflow.sh
@@ -72,9 +72,9 @@ scrfunc_dir=$( dirname "${scrfunc_fp}" )
 exptdir=$( dirname "$0" )
 if [[ $(uname -s) == Darwin ]]; then
   command -v greadlink >/dev/null 2>&1 || { echo >&2 "For Darwin-based operating systems (MacOS), the 'greadlink' utility is required to run the UFS SRW Application. Reference the User's Guide for more information about platform requirements. Aborting."; exit 1; }
-  scrfunc_fp=$( greadlink -f "$exptdir" )
+  exptdir=$( greadlink -f "$exptdir" )
 else
-  scrfunc_fp=$( readlink -f "$exptdir" )
+  exptdir=$( readlink -f "$exptdir" )
 fi
 #
 #-----------------------------------------------------------------------
