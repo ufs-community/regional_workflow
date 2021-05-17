6e19b591c80ceff996336da76294c58141e459f1
Chan-hoo.Jeon <chan-hoo.jeon@noaa.gov>
2021-05-17 22:39:12 +0000

    Modify scripts

--- ush/config_defaults.sh
+++ ush/config_defaults.sh
@@ -1721,6 +1721,6 @@ MAXTRIES_RUN_NEXUS="4"
 CYCL_INC="00"
 RESTART_WORKFLOW="FALSE"
 RESTART_CYCLE_DIR="/path/to/restart/cycle/dir"
-RESTART_INTERVAL=""
+RESTART_INTERVAL="0"
 #
 ##### RRFS-CMAQ ########## end   #####
