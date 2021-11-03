90f8f9b39b5c076a3b7a6052ee98583359e39e52
Michael Kavulich, Jr <kavulich@ucar.edu>
2021-11-03 13:31:47 +0000

    Major upgrade of grid_diag and plotting scripts, allow arbitrary variables without constantly tweaking things, also plotting script now mostly runs on command-line arguments

--- ush/grid_diag_scripts/plot_grid_diag_2dhist.py
+++ ush/grid_diag_scripts/plot_grid_diag_2dhist.py
@@ -7,41 +7,116 @@ from matplotlib.colors import BoundaryNorm
 ## Added for logarithmic color scale
 import matplotlib.colors as colors
 
-#start_date = '2019041800'
-#end_date = '2019041800'
+import argparse
+import sys
 
-start_date=os.environ.get('INIT_BEG')
-end_date=os.environ.get('INIT_END')
+# Parse input arguments
+parser = argparse.ArgumentParser()
 
-start_date = '2019041500'
-end_date = '2019053000'
+parser.add_argument("-env", "--use_environment", help="If set, uses environment variables rather than arguments to set necessary variables", action="store_true")
 
-start_fhr = '12'
-end_fhr = '36'
+parser.add_argument("-v1", "--var1", help="Variable 1, required unless '-env' flag used", type = str)
+parser.add_argument("-v2", "--var2", help="Variable 2, required unless '-env' flag used", type = str)
+parser.add_argument("-u1", "--var1_units", help="Variable 1 units (for plot axes)", type = str, default='')
+parser.add_argument("-u2", "--var2_units", help="Variable 2 units (for plot axes)", type = str, default='')
+parser.add_argument("-s", "--start_date", help="Start date, required unless '-env' flag used", type = str)
+parser.add_argument("-e", "--end_date", help="End date, required unless '-env' flag used", type = str)
+parser.add_argument("-sfhr", "--start_fhr", help="Start forecast hour, required unless '-env' flag used", type = str)
+parser.add_argument("-efhr", "--end_fhr", help="End forecast hour, required unless '-env' flag used", type = str)
+parser.add_argument("-ob", "--output_base", help="Base directory for METplus output (where nc file is located)", type = str)
+parser.add_argument("-m", "--model", help="'Model name' specified in METplus batch script", type = str)
+parser.add_argument("-v1l", "--var1_lev", help="Variable 1 level as defined in grib2 output", type = str, default='L0')
+parser.add_argument("-v2l", "--var2_lev", help="Variable 2 level as defined in grib2 output", type = str, default='L0')
+args = parser.parse_args()
 
-#grid_diag_out = os.environ.get('OUTPUT_BASE') + 'grid_diag_out_' + os.environ.get('MODEL') + '_' + os.environ.get('VAR1') + '-' + os.environ.get('VAR3') + '_' + start_date + '-' + end_date + '_f' + start_fhr + '-' + end_fhr + '.nc'
-#grid_diag_out = '/scratch2/BMC/fv3lam/RRFS_baseline/expt_dirs/RRFS_baseline_summer/GridDiag/grid_diag_out_FV3_RRFS_v1alpha_3km_summer_' + start_date + '-' + end_date + '_f' + start_fhr + '-' + end_fhr + '.nc'
-grid_diag_out = '/scratch2/BMC/fv3lam/RRFS_baseline/expt_dirs/RRFS_baseline_summer/GridDiag/grid_diag_out_FV3_RRFS_v1alpha_3km_summer_2019041500-2019053000_f12-36.nc'
+output_base = None
+model = None
+if args.use_environment:
+   
+   var1 = os.environ.get('VAR1')
+   var2 = os.environ.get('VAR2')
+   start_date = os.environ.get('START_DATE')
+   end_date = os.environ.get('END_DATE')
+   start_fhr = os.environ.get('FHR_FIRST')
+   end_fhr = os.environ.get('FHR_LAST')
+   var1_units = os.environ.get('VAR1_UNITS')
+   var2_units = os.environ.get('VAR2_UNITS')
+   output_base = os.environ.get('OUTPUT_BASE')
+   model = os.environ.get('MODEL')
+   var1_lev = os.environ.get('VAR1_LEV')
+   var2_lev = os.environ.get('VAR2_LEV')
+ 
+   if var1 is None or var2 is None or start_date is None or end_date is None or start_fhr is None or end_fhr is None:
+      print("ERROR: If -env flag is set, the following environment variables must be set:")
+      print("VAR1, VAR2, START_DATE, END_DATE, START_FHR, END_FHR")
+      sys.exit("Exiting")
+
+else:
+   if args.var1 is None or args.var2 is None or args.start_date is None or args.end_date is None or args.start_fhr is None or args.end_fhr is None:
+      sys.exit("ERROR: Unless -env flag is set, you must provide all arguments")
+
+   var1        = args.var1
+   var2        = args.var2
+   start_date  = args.start_date
+   end_date    = args.end_date
+   start_fhr   = args.start_fhr
+   end_fhr     = args.end_fhr
+   var1_units  = args.var1_units
+   var2_units  = args.var2_units
+   output_base = args.output_base
+   model       = args.model
+   var1_lev    = args.var1_lev
+   var2_lev    = args.var2_lev
+
+print("var1       = " + var1)
+print("var2       = " + var2)
+print("start_date = " + start_date)
+print("end_date   = " + end_date)
+print("start_fhr  = " + start_fhr)
+print("end_fhr    = " + end_fhr)
+print("var1_units = " + var1_units)
+print("var2_units = " + var2_units)
+
+
+
+if output_base is None or model is None:
+   print("Output base and/or model have not been specified; falling back to METplus default path and filename")
+   grid_diag_out = '/scratch2/BMC/fv3lam/RRFS_baseline/expt_dirs/RRFS_baseline_summer/GridDiag/grid_diag_out_FV3_RRFS_v1alpha_3km_summer_' + start_date + '-' + end_date + '_f' + start_fhr + '-' + end_fhr + '.nc'
+
+else:
+   grid_diag_out = output_base + '/grid_diag_out_' + model + '_' + var1 + '-' + var2 + '_' + start_date + '-' + end_date + '_f' + start_fhr + '-' + end_fhr + '.nc'
+
+print("grid_diag_out = " + grid_diag_out)
 
 file_id = Dataset(grid_diag_out, 'r')
-hist_gfs_obs = file_id.variables['hist_RETOP_L0_EchoTop18_Z500'][:]
-gfs_ctr = file_id.variables['RETOP_L0_mid'][:]
-obs_ctr = file_id.variables['EchoTop18_Z500_mid'][:]
+hist_str = 'hist_' + var1 + '_' + var1_lev + '_' + var2 + '_' + var2_lev
+var1_ctr_str = var1 + '_' + var1_lev + '_mid'
+var2_ctr_str = var2 + '_' + var2_lev + '_mid'
+
+hist_var1_var2 = file_id.variables[hist_str][:]
+var1_ctr = file_id.variables[var1_ctr_str][:]
+var2_ctr = file_id.variables[var2_ctr_str][:]
+
+print("hist_var1_var2 = " + hist_str)
+print("var1_ctr       = " + var1_ctr_str)
+print("var2_ctr       = " + var2_ctr_str)
+
+
 file_id.close()
 #MergedReflectivityQCComposite_00.50_20191201-010000.grib2
 #EchoTop_18_00.50_20191201-000000.grib2
-n_total = np.sum(hist_gfs_obs)
+n_total = np.sum(hist_var1_var2)
 print(n_total)
 
-obs_mesh, gfs_mesh = np.meshgrid(obs_ctr, gfs_ctr)
-gfs_count = np.sum(hist_gfs_obs, axis=0)
-obs_count = np.sum(hist_gfs_obs, axis=1)
-#print(gfs_count)
-#print(obs_count)
+var2_mesh, var1_mesh = np.meshgrid(var2_ctr, var1_ctr)
+var1_count = np.sum(hist_var1_var2, axis=0)
+var2_count = np.sum(hist_var1_var2, axis=1)
+#print(var1_count)
+#print(var2_count)
 
-gfs_mean = np.sum(
-    gfs_mesh * hist_gfs_obs, axis=0) \
-    / gfs_count
+var1_mean = np.sum(
+    var1_mesh * hist_var1_var2, axis=0) \
+    / var1_count
 
 
 # plot
@@ -51,25 +126,25 @@ data_max=750000
 cmap = plt.get_cmap('nipy_spectral')
 levels = np.linspace(0.0, data_max, 51)
 norm = BoundaryNorm(levels, ncolors=cmap.N, clip=True)
-##hist = plt.pcolormesh(obs_mesh, gfs_mesh,
-##    hist_gfs_obs,
+##hist = plt.pcolormesh(var2_mesh, var1_mesh,
+##    hist_var1_var2,
 ##    cmap=cmap, norm=norm)
-hist_gfs_obs_norm = 100*hist_gfs_obs / np.sum(np.sum(hist_gfs_obs))
-hist = plt.pcolormesh(obs_mesh, gfs_mesh,
-    hist_gfs_obs_norm,
+hist_var1_var2_norm = 100*hist_var1_var2 / np.sum(np.sum(hist_var1_var2))
+hist = plt.pcolormesh(var2_mesh, var1_mesh,
+    hist_var1_var2_norm,
     cmap = cmap, norm = colors.LogNorm(vmin = 0.00001, vmax = 0.05))
 ##
-# hist_gfs_obs / n_total
+# hist_var1_var2 / n_total
 ax = plt.gca()
 
 ## Increased linewidth from 1 to 2
-#ax.plot(obs_ctr, gfs_mean, linewidth=2)
+#ax.plot(var2_ctr, var1_mean, linewidth=2)
 ## Add grid lines 
 ax.grid(True, which = 'major', axis = 'both', linestyle = '--', 
         color = (0.5, 0.5, 0.5))
 ##
-ax.set_xlabel('RETOP obs' + ' ')
-ax.set_ylabel('RETOP model' + ' ')
+ax.set_ylabel(args.var1 + ' ' + args.var1_units)
+ax.set_xlabel(args.var2 + ' ' + args.var2_units)
 plt.xlim(0, 70)
 plt.ylim(0, 70)
 plt.title('MET GridDiag 2D Histogram')
@@ -79,7 +154,11 @@ cbar = plt.colorbar()
 cbar.ax.set_ylabel('Probability (%)')
 ##
 
+plotname='2dhist_' + var1 + var1_lev + '_' + var2 + var2_lev + '_' + start_date + '_' + end_date + '_fhr' + start_fhr + '-' + end_fhr
+index=0
+if os.path.exists(plotname + '.png'):
+    while os.path.exists(plotname + '_' + str(index) + '.png'):
+        index += 1
+    plotname = plotname + '_' + str(index)
 
-#plt.savefig('2dhist_' + varname + start_date + '_' + end_date + '_fhr' + start_fhr + '-' + end_fhr + '.png', dpi=300)
-plt.savefig('2dhist_test.png', dpi=300)
-
+plt.savefig(plotname + '.png', dpi=300)
---
+++ ush/grid_diag_scripts/run_metplus_griddiag_var-var.ksh
@@ -0,0 +1,86 @@
+#!/bin/ksh
+#SBATCH --account=fv3lam
+#SBATCH --qos=batch
+#SBATCH --partition=hera
+#SBATCH --nodes=1-1
+#SBATCH --tasks-per-node=1
+#SBATCH -t 00:20:00
+#SBATCH --job-name=metplus_griddiag
+#SBATCH -o run_metplus_griddiag_var-var.log
+
+# Set up environment on Hera 
+module purge
+module use -a /contrib/anaconda/modulefiles
+module load intel/18.0.5.274
+module load anaconda/latest
+module use -a /contrib/met/modulefiles/
+module load met/10.0.0
+module use /contrib/METplus/modulefiles
+module load metplus/4.0.0
+
+# Set paths
+MET_INSTALL_DIR=/contrib/met/10.0.0
+MET_BIN_EXEC=bin
+MET_CONFIG=/scratch2/BMC/fv3lam/RRFS_baseline/ufs-srweather-app/regional_workflow/ush/templates/parm/met
+METPLUS_PATH=/contrib/METplus/METplus-4.0.0
+METPLUS_CONF=/scratch2/BMC/fv3lam/RRFS_baseline/ufs-srweather-app/regional_workflow/ush/templates/parm/metplus
+
+INPUT_BASE=/scratch2/BMC/fv3lam/RRFS_baseline
+OUTPUT_BASE=/scratch2/BMC/fv3lam/RRFS_baseline/expt_dirs/RRFS_baseline_summer/GridDiag
+
+export VAR1='RETOP'
+export VAR1_OPTS='convert(x) = x * 3.28084 * 0.001; n_bins = 60; range  = [0, 60]; cnt_thresh = [ >15 ];'
+export VAR1_UNITS='kft'
+export VAR1_LEV='L0'
+
+export VAR2='REFC'
+export VAR2_OPTS='cnt_thresh = [ >15 ]; n_bins = 70; range  = [0, 70];'
+export VAR2_UNITS='dBz'
+export VAR2_LEV='L0'
+
+export MET_INSTALL_DIR
+export MET_BIN_EXEC
+export METPLUS_PATH
+export METPLUS_CONF
+export MET_CONFIG
+
+export INPUT_BASE
+export OUTPUT_BASE
+export OBS_DIR
+
+# Set dates/times to process
+INIT_BEG=2019041500 #2019041500
+INIT_END=2019053000 #2019053000
+INIT_INC=259200 #in seconds (3 days)
+
+FHR_FIRST=12 #00
+FHR_LAST=36 #36
+FHR_INC=1 #hourly
+
+export INIT_BEG
+export INIT_END
+export INIT_INC
+export FHR_FIRST
+export FHR_LAST
+export FHR_INC
+
+MODEL=FV3_RRFS_v1alpha_3km_summer
+NET=rrfs
+export MODEL
+export NET
+
+export SEASON=summer
+
+echo "Go to OUTPUT_BASE:${OUTPUT_BASE}"
+cd ${OUTPUT_BASE}
+
+${METPLUS_PATH}/ush/master_metplus.py \
+  -c ${METPLUS_CONF}/common.conf \
+  -c ${METPLUS_CONF}/GridDiag_2vars.conf
+
+echo "METplus complete, run this command to generate graphics:"
+echo ""
+
+echo "python3 plot_grid_diag_2dhist.py -v1=$VAR1 -v2=$VAR2 -v1l=$VAR1_LEV -v2l=$VAR2_LEV -u1=$VAR1_UNITS -u2=$VAR2_UNITS -s=$INIT_BEG -e=$INIT_END -sfhr=$FHR_FIRST -efhr=$FHR_LAST -ob=$OUTPUT_BASE -m=$MODEL"
+
+echo "Done!"
