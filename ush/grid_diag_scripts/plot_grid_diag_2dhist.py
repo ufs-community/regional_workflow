e4bb0518897dcb9aa758614adc62df8f2282cc2c
Michael Kavulich, Jr <kavulich@ucar.edu>
2021-11-11 05:45:21 +0000

    Remove old, unused environment option from python plotting script; add helpful print statements and remove unhelpful ones

--- ush/grid_diag_scripts/plot_grid_diag_2dhist.py
+++ ush/grid_diag_scripts/plot_grid_diag_2dhist.py
@@ -13,16 +13,14 @@ import sys
 # Parse input arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument("-env", "--use_environment", help="If set, uses environment variables rather than arguments to set necessary variables", action="store_true")
-
-parser.add_argument("-v1", "--var1", help="Variable 1, required unless '-env' flag used", type = str)
-parser.add_argument("-v2", "--var2", help="Variable 2, required unless '-env' flag used", type = str)
+parser.add_argument("-v1", "--var1", help="Variable 1", type = str, required=True)
+parser.add_argument("-v2", "--var2", help="Variable 2", type = str, required=True)
 parser.add_argument("-u1", "--var1_units", help="Variable 1 units (for plot axes)", type = str, default='')
 parser.add_argument("-u2", "--var2_units", help="Variable 2 units (for plot axes)", type = str, default='')
-parser.add_argument("-s", "--start_date", help="Start date, required unless '-env' flag used", type = str)
-parser.add_argument("-e", "--end_date", help="End date, required unless '-env' flag used", type = str)
-parser.add_argument("-sfhr", "--start_fhr", help="Start forecast hour, required unless '-env' flag used", type = str)
-parser.add_argument("-efhr", "--end_fhr", help="End forecast hour, required unless '-env' flag used", type = str)
+parser.add_argument("-s", "--start_date", help="Start date of experiement (initial time of first forecast)", type = str, required=True)
+parser.add_argument("-e", "--end_date", help="End date of experiment (initial time of final forecast)", type = str, required=True)
+parser.add_argument("-sfhr", "--start_fhr", help="Start forecast hour", type = str, required=True)
+parser.add_argument("-efhr", "--end_fhr", help="End forecast hour", type = str, required=True)
 parser.add_argument("-ob", "--output_base", help="Base directory for METplus output (where nc file is located)", type = str)
 parser.add_argument("-m", "--model", help="'Model name' specified in METplus batch script", type = str)
 parser.add_argument("-v1l", "--var1_lev", help="Variable 1 level as defined in grib2 output", type = str, default='L0')
@@ -31,52 +29,18 @@ args = parser.parse_args()
 
 output_base = None
 model = None
-if args.use_environment:
-   
-   var1 = os.environ.get('VAR1')
-   var2 = os.environ.get('VAR2')
-   start_date = os.environ.get('START_DATE')
-   end_date = os.environ.get('END_DATE')
-   start_fhr = os.environ.get('FHR_FIRST')
-   end_fhr = os.environ.get('FHR_LAST')
-   var1_units = os.environ.get('VAR1_UNITS')
-   var2_units = os.environ.get('VAR2_UNITS')
-   output_base = os.environ.get('OUTPUT_BASE')
-   model = os.environ.get('MODEL')
-   var1_lev = os.environ.get('VAR1_LEV')
-   var2_lev = os.environ.get('VAR2_LEV')
- 
-   if var1 is None or var2 is None or start_date is None or end_date is None or start_fhr is None or end_fhr is None:
-      print("ERROR: If -env flag is set, the following environment variables must be set:")
-      print("VAR1, VAR2, START_DATE, END_DATE, START_FHR, END_FHR")
-      sys.exit("Exiting")
-
-else:
-   if args.var1 is None or args.var2 is None or args.start_date is None or args.end_date is None or args.start_fhr is None or args.end_fhr is None:
-      sys.exit("ERROR: Unless -env flag is set, you must provide all arguments")
-
-   var1        = args.var1
-   var2        = args.var2
-   start_date  = args.start_date
-   end_date    = args.end_date
-   start_fhr   = args.start_fhr
-   end_fhr     = args.end_fhr
-   var1_units  = args.var1_units
-   var2_units  = args.var2_units
-   output_base = args.output_base
-   model       = args.model
-   var1_lev    = args.var1_lev
-   var2_lev    = args.var2_lev
-
-print("var1       = " + var1)
-print("var2       = " + var2)
-print("start_date = " + start_date)
-print("end_date   = " + end_date)
-print("start_fhr  = " + start_fhr)
-print("end_fhr    = " + end_fhr)
-print("var1_units = " + var1_units)
-print("var2_units = " + var2_units)
-
+var1        = args.var1
+var2        = args.var2
+start_date  = args.start_date
+end_date    = args.end_date
+start_fhr   = args.start_fhr
+end_fhr     = args.end_fhr
+var1_units  = args.var1_units
+var2_units  = args.var2_units
+output_base = args.output_base
+model       = args.model
+var1_lev    = args.var1_lev
+var2_lev    = args.var2_lev
 
 
 if output_base is None or model is None:
@@ -86,14 +50,14 @@ if output_base is None or model is None:
 else:
    grid_diag_out = output_base + '/grid_diag_out_' + model + '_' + var1 + '_' + var1_lev + '-' + var2 + '_' + var2_lev + '_' + start_date + '-' + end_date + '_f' + start_fhr + '-' + end_fhr + '.nc'
 
-print("grid_diag_out = " + grid_diag_out)
-
 #From here on, if we need "APCP" it should be read as "APCP_00"
 if var1 == "APCP":
    var1 = "APCP_00"
 
 if var2 == "APCP":
    var2 = "APCP_00"
+
+print('Reading GridDiag out file ' + grid_diag_out)
 
 file_id = Dataset(grid_diag_out, 'r')
 hist_str = 'hist_' + var1 + '_' + var1_lev + '_' + var2 + '_' + var2_lev
@@ -104,28 +68,19 @@ hist_var1_var2 = file_id.variables[hist_str][:]
 var1_ctr = file_id.variables[var1_ctr_str][:]
 var2_ctr = file_id.variables[var2_ctr_str][:]
 
-print("hist_var1_var2 = " + hist_str)
-print("var1_ctr       = " + var1_ctr_str)
-print("var2_ctr       = " + var2_ctr_str)
-
-
 file_id.close()
-#MergedReflectivityQCComposite_00.50_20191201-010000.grib2
-#EchoTop_18_00.50_20191201-000000.grib2
 n_total = np.sum(hist_var1_var2)
-print(n_total)
 
 var2_mesh, var1_mesh = np.meshgrid(var2_ctr, var1_ctr)
 var1_count = np.sum(hist_var1_var2, axis=0)
 var2_count = np.sum(hist_var1_var2, axis=1)
-#print(var1_count)
-#print(var2_count)
 
 var1_mean = np.sum(
     var1_mesh * hist_var1_var2, axis=0) \
     / var1_count
 
 
+print('Creating plot of 2d histogram')
 # plot
 #data_max=100000
 data_max=750000
@@ -168,4 +123,6 @@ if os.path.exists(plotname + '.png'):
         index += 1
     plotname = plotname + '_' + str(index)
 
+print('Saving plot to file ' + plotname + '.png')
 plt.savefig(plotname + '.png', dpi=300)
+print('Plot created successfully!')
