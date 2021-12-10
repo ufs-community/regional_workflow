a525cb5398969e26f20a99d6b0b262e969f39a95
gerard ketefian <gerard.ketefian@noaa.gov>
2021-12-10 13:29:40 -0700

    In constants.sh, add the variable VAR_REF_CHAR that contains the character that indicates a reference to a bash variable in a string (this character is the dollar sign).  Then, in check_var_valid_value.sh, use this new variable to skip checking for valid values if a variable's value contains this character.

--- ush/constants.sh
+++ ush/constants.sh
@@ -15,3 +15,19 @@ degs_per_radian=$( bc -l <<< "360.0/(2.0*$pi_geom)" )
 # Radius of the Earth in meters.
 radius_Earth="6371200.0"
 
+#
+#-----------------------------------------------------------------------
+#
+# Character used to identify a reference (in bash) to a variable in a 
+# given string.  In bash, this is the dollar sign.  For example, if the
+# string is
+#
+#   "Hello, my name is $NAME."
+#
+# then the dollar sign indicates that $NAME is a reference to the contents
+# of the variable NAME.
+#
+#-----------------------------------------------------------------------
+#
+VAR_REF_CHAR="\$"
+
