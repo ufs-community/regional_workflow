f0b6df1c0279d702adc02c5bd2439823dfcd6cd5
Christina.Holt <Christina.Holt@noaa.gov>
2021-12-15 19:25:41 +0000

    Add offset hours to defaults file.

--- ush/config_defaults.sh
+++ ush/config_defaults.sh
@@ -570,6 +570,22 @@ NDAS_OBS_DIR="/path/to/observation-directory/ndas/proc"
 # data availble at least every 6 hours.  It is up to the user to ensure 
 # that this is the case.
 #
+# EXTRN_MDL_ICS_OFFSET_HRS:
+# Users may wish to start a forecast from a forecast of a previous cycle
+# of an external model. This variale sets the number of hours earlier
+# the external model started than when the FV3 forecast configured here
+# should start. For example, the forecast should start from a 6 hour
+# forecast of the GFS, then EXTRN_MDL_ICS_OFFSET_HRS=6.
+
+# EXTRN_MDL_LBCS_OFFSET_HRS:
+# Users may wish to use lateral boundary conditions from a forecast that
+# was started earlier than the initial time for the FV3 forecast
+# configured here. This variale sets the number of hours earlier
+# the external model started than when the FV3 forecast configured here
+# should start. For example, the forecast should use lateral boundary
+# conditions from the GFS started 6 hours earlier, then
+# EXTRN_MDL_LBCS_OFFSET_HRS=6.
+#
 # FV3GFS_FILE_FMT_ICS:
 # If using the FV3GFS model as the source of the ICs (i.e. if EXTRN_MDL_NAME_ICS
 # is set to "FV3GFS"), this variable specifies the format of the model
@@ -585,6 +601,8 @@ NDAS_OBS_DIR="/path/to/observation-directory/ndas/proc"
 EXTRN_MDL_NAME_ICS="FV3GFS"
 EXTRN_MDL_NAME_LBCS="FV3GFS"
 LBC_SPEC_INTVL_HRS="6"
+EXTRN_MDL_ICS_OFFSET_HRS="0"
+EXTRN_MDL_LBCS_OFFSET_HRS="0"
 FV3GFS_FILE_FMT_ICS="nemsio"
 FV3GFS_FILE_FMT_LBCS="nemsio"
 #
