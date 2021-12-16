dd5f7641d16e6321577a5ab3c57412d7a81eda2c
gerard ketefian <gerard.ketefian@noaa.gov>
2021-12-16 11:42:06 -0700

    Clean up comments regarding use of templates in the WE2E configuration file for templates.  We trim most of the comments since there will be a companion PR into ufs-srweather-app that will include RST documentation for templates.

--- tests/WE2E/test_configs/wflow_features/config.deactivate_tasks.sh
+++ tests/WE2E/test_configs/wflow_features/config.deactivate_tasks.sh
@@ -50,66 +50,13 @@ RUN_TASK_RUN_POST="FALSE"
 #
 # The following shows examples of how to define template variables.  Here,
 # we define RUN_CMD_UTILS, RUN_CMD_FCST, and RUN_CMD_POST as template 
-# variables.  Note that these variables aren't actually needed to run
-# any commands but are simply included for demonstration purposes.
+# variables.  Note that during this test, these templates aren't actually 
+# expanded/used (using bash's "eval" built-in command) anywhere in the 
+# scripts.  They are included here simply included for demonstration 
+# purposes.  If all goes well, the experiment generation system should 
+# place these templates at the end of the experiment's variable definitions 
+# file.
 #
-# If we want the contents in RUN_CMD_UTILS, RUN_CMD_FCST, and RUN_CMD_POST 
-# to be expanded and/or executed when the variable definitions file 
-# (GLOBAL_VAR_DEFNS_FN) is sourced in a script or function, then we must
-# escape bash's variable reference character "$" using backslashes, as 
-# follows:
-#
-#   RUN_CMD_UTILS="echo \$yyyymmdd"
-#   RUN_CMD_FCST="mpirun -np \${PE_MEMBER01}"
-#   RUN_CMD_POST="\$( echo hello \$yyymmdd )"
-#
-# With this method, when the variable defintions file is sourced, the 
-# variable references on the right-hand sides (here $yyyymmdd and 
-# ${PE_MEMBER01}) as well as the command substitution [i.e. $( echo ...)]
-# will first be evaluated/expanded and the results then combined with 
-# the remainder of each string on the right-hand side (if any) to obtain 
-# literal strings to assign to RUN_CMD_UTILS, RUN_CMD_FCST, and RUN_CMD_POST.  
-# Thus, the values that these three variables are assigned in the variable
-# definitions file will not contain any variable references; they will
-# contain only literal strings.
-#
-# In general, this is not what we want because the variables on the right-
-# hand sides may not yet be defined at the time the variable definitions
-# file is sourced.  Thus, what we usually want is for variable references
-# and command substitutions appearing on the right-hand sides in that 
-# file to be escaped.  To do this, we must escape the "$" character twice, 
-# as follows:
-#
-#   RUN_CMD_UTILS="echo \\\$yyyymmdd"
-#   RUN_CMD_FCST="mpirun -np \\\${PE_MEMBER01}"
-#   RUN_CMD_POST="\\\$( echo hello \\\$yyyymmdd )"
-#
-# A more compact way to do this is to use single quotes on the outside
-# and eliminate the escaped-\ within, i.e.
-#
-#   RUN_CMD_UTILS='echo \$yyyymmdd'
-#   RUN_CMD_FCST='mpirun -np \${PE_MEMBER01}'
-#   RUN_CMD_POST='\$( echo hello \$yyyymmdd )'
-#
-# This will cause $yyyymmdd, ${PE_MEMBER01}, and $(...) to be expanded
-# only at the time RUN_CMD_UTILS, RUN_CMD_FCST, and RUN_CMD_POST are 
-# referenced in a file or script (using the syntax ${RUN_CMD_UTILS}, ...)
-# and not earlier when the variable definitions file is sourced.  That
-# is what we do in this WE2E test.
-#
-# With the last set of settings above, in a given script or function we 
-# can source the variable defintions file and then use the three variables 
-# as follows:
-#
-#   . $exptdir/var_defns.sh
-#   yyyymmdd="goodbye"
-#   eval ${RUN_CMD_UTILS}
-#   eval ${RUN_CMD_FCST}
-#   eval myvar=${RUN_CMD_POST}
-#
-# (The mpirun command in RUN_CMD_FCST will generate an error, but that is
-# ok since using mpirun is not the primary concern here.)
-#
-RUN_CMD_UTILS='echo \$yyyymmdd'
+RUN_CMD_UTILS='cd \$yyyymmdd'
 RUN_CMD_FCST='mpirun -np \${PE_MEMBER01}'
 RUN_CMD_POST='\$( echo hello \$yyyymmdd )'
