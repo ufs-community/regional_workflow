b3ad4a7ed46a4f9916595566f8b46856aeae729b
Christina.Holt <Christina.Holt@noaa.gov>
2022-01-14 19:24:51 +0000

    Adding capability to copy files from disk.

--- ush/retrieve_data.py
+++ ush/retrieve_data.py
@@ -20,6 +20,33 @@ import subprocess
 import yaml
 
 
+def copy_file(source, destination):
+
+    '''
+    Copy a file from a source and place it in the destination location.
+    Return a boolean value reflecting the state of the copy.
+
+    Assumes destination exists.
+    '''
+
+    if not os.path.exists(source):
+        logging.info(f'File does not exist on disk \n {source}')
+        return False
+
+    # Using subprocess here because system copy is much faster than
+    # python copy options.
+    cmd = f'cp {source} {destination}'
+    logging.info(f'Running command: \n {cmd}')
+    try:
+        output = subprocess.run(cmd,
+                                check=True,
+                                shell=True,
+                                )
+    except subprocess.CalledProcessError as err:
+        logging.info(err)
+        return False
+    return True
+
 def download_file(url):
 
     '''
@@ -27,8 +54,6 @@ def download_file(url):
     on disk.
 
     Arguments:
-      target_path  full path to a location on disk where file will be
-                   stored
       url          url to file to be downloaded
 
     Return:
@@ -164,6 +189,69 @@ def fill_template(template_str, cycle_date, fcst_hr=0):
         yyyymmddhh=cycle_date.strftime('%Y%m%d%H'),
         )
 
+def get_requested_files(cla, file_names, input_locs, method='disk'):
+
+    ''' This function copies files from disk locations
+    or downloads files from a url, depending on the option specified for
+    user.
+
+    This function expects that the output directory exists and is
+    writeable.
+
+    Arguments:
+
+    cla        Namespace object containing command line arguments
+    file_names Dict of file names by file type and kind
+    input_locs List of potential data locations, either urls or disk
+               paths.
+    method     Choice of disk or download to indicate protocol for
+               retrieval
+
+    Returns
+    unavailable  a dict whose keys are "method" and whose values are a
+                 list of files unretrievable
+    '''
+
+    unavailable = {}
+
+    if cla.file_type is not None:
+        file_names = file_names[cla.file_type]
+    file_names = file_names[cla.anl_or_fcst]
+
+    file_names = file_names if isinstance(file_names, list) else \
+            [file_names]
+    target_path = fill_template(cla.output_path,
+                                cla.cycle_date)
+
+    logging.info(f'Retrieved files will be placed here: \n {target_path}')
+    orig_path = os.getcwd()
+    os.chdir(target_path)
+    unavailable = {}
+    for input_loc in input_locs:
+        for fcst_hr in cla.fcst_hrs:
+            for file_name in file_names:
+                loc = os.path.join(input_loc, file_name)
+                loc = fill_template(loc, cla.cycle_date, fcst_hr)
+
+                if method == 'disk':
+                    retrieved = copy_file(loc, target_path)
+  
+                if method == 'download':
+                    retrieved = download_file(loc)
+
+                if not retrieved:
+
+                    if unavailable.get(method) is None:
+                        unavailable[method] = []
+                    unavailable[method].append(target_path)
+                    os.chdir(orig_path)
+                    # Returning here assumes that if the first file
+                    # isn't found, none of the others will be. Don't
+                    # waste time timing out on every requested file.
+                    return unavailable
+    os.chdir(orig_path)
+    return unavailable
+
 def hsi_single_file(file_path, mode='ls'):
 
     ''' Call hsi as a subprocess for Python and return information about
@@ -194,9 +282,12 @@ def hpss_requested_files(cla, store_specs):
 
     ''' This function interacts with the "hpss" protocol in a
     provided data store specs file to download a set of files requested
-    by the user. It calls retrieve_tar for each individual file that
-    should be fetched and then untar_file to stage individual files on
-    disk.
+    by the user. Depending on the type of archive file (zip or tar), it
+    will either pull the entire file and unzip it, or attempt to pull
+    individual files from a tar file.
+
+    It cleans up local disk after files are deemed available to remove
+    any empty subdirectories that may still be present.
 
     This function exepcts that the output directory exists and is
     writable.
@@ -314,7 +405,12 @@ def hpss_requested_files(cla, store_specs):
 
     os.chdir(orig_path)
 
-def load_config(arg):
+def load_str(arg):
+
+    ''' Load a dict string safely using YAML. Return the resulting dict.  '''
+    return yaml.load(arg, Loader=yaml.SafeLoader)
+
+def config_exists(arg):
 
     '''
     Check to ensure that the provided config file exists. If it does, load it
@@ -382,16 +478,42 @@ def main(cla):
     unavailable = {}
     for data_store in cla.data_stores:
         logging.info(f'Checking {data_store} for {cla.external_model}')
-        store_specs = known_data_info.get(data_store)
+        store_specs = known_data_info.get(data_store, {})
 
-        if store_specs is None:
+        if data_store == 'disk':
+            file_names = cla.file_names if cla.file_names else \
+                known_data_info.get('hpss', {}).get('file_names')
+            logging.debug(f'User supplied file names are: {file_names}')
+            if not file_names:
+                msg = ('No file name found. They must be provided \
+                        either on the command line or on in a config file.')
+                raise argparse.ArgumentTypeError(msg)
+            unavailable = get_requested_files(cla,
+                                              file_names=file_names,
+                                              input_locs=cla.input_file_paths,
+                                              method='disk',
+                                              )
+
+        elif not store_specs:
             msg = (f'No information is available for {data_store}.')
             raise KeyError(msg)
 
-        if store_specs['protocol'] == 'download':
-            unavailable = download_requested_files(cla, data_store, store_specs)
+        if store_specs.get('protocol') == 'download':
+            file_names = store_specs.get('file_names')
+            if not file_names:
+                msg = ('No file name found. They must be provided \
+                        either on the command line or on in a config file.')
+                raise argparse.ArgumentTypeError(msg)
 
-        if store_specs['protocol'] == 'htar':
+            input_locs = store_specs['url']
+            input_locs = input_locs if isinstance(input_locs, list) else [input_locs]
+            unavailable = get_requested_files(cla,
+                                              file_names=file_names,
+                                              input_locs=input_locs,
+                                              method='download',
+                                              )
+
+        if store_specs.get('protocol') == 'htar':
             unavailable = hpss_requested_files(cla, store_specs)
 
         if not unavailable:
@@ -423,6 +545,11 @@ def parse_args():
         help='Flag for whether analysis or forecast \
         files should be gathered',
         required=True,
+        )
+    parser.add_argument(
+        '--config',
+        help='Full path to YAML with known data information',
+        type=config_exists,
         )
     parser.add_argument(
         '--cycle_date',
@@ -465,19 +592,29 @@ def parse_args():
 
     # Optional
     parser.add_argument(
-        '--config',
-        help='Full path to YAML with known data information',
-        type=load_config,
-        )
-    parser.add_argument(
         '--debug',
         action='store_true',
         help='Print debug messages',
         )
     parser.add_argument(
+        '--file_names',
+        help='Command-line user config options in YAML-formatted \
+        string to indicate file names on disk that are not \
+        consistent with the hpss archive, which are used when \
+        this flag isn\'t used.',
+        type=load_str,
+        )
+    parser.add_argument(
         '--file_type',
         choices=('grib2', 'nemsio', 'netcdf'),
         help='External model file format',
+        )
+    parser.add_argument(
+        '--input_file_paths',
+        help='Python templates known data input path. File names are ' \
+        'used from file provided by --config file. One path is needed ' \
+        'for each type of file requested.',
+        nargs='*',
         )
     return parser.parse_args()
 
@@ -486,4 +623,12 @@ if __name__ == '__main__':
     CLA = parse_args()
     CLA.output_path = path_exists(CLA.output_path)
     CLA.fcst_hrs = fhr_list(CLA.fcst_hrs)
+
+    if 'disk' in CLA.data_stores:
+        # Make sure a path was provided.
+        if not CLA.input_file_paths:
+            msg = ('You must provide an input_file_path when choosing ' \
+                   ' disk as a data store!')
+            raise argparse.ArgumentTypeError(msg)
+
     main(CLA)
