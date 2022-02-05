35835878ee1cee1ed87792cb1419f5c891b637fc
Daniel Abdi <daniel.abdi@noaa.gov>
2022-02-05 20:55:44 +0000

    Add more unit tests.

--- ush/create_diag_table_file.py
+++ ush/create_diag_table_file.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import unittest
 from textwrap import dedent
 
-from python_utils import process_args, import_vars, set_env_var, \
-                         print_input_args, define_macro_utilities
+from python_utils import process_args, import_vars, set_env_var, print_input_args, \
+                         run_command, print_info_msg, print_err_msg_exit, type_to_str
 
 def create_diag_table_file(**kwargs):
     """ Creates a diagnostic table file for each cycle to be run
@@ -12,7 +13,7 @@ def create_diag_table_file(**kwargs):
     Args:
         run_dir: run directory
     Returns:
-        None
+        Boolean
     """
 
     #process input arguments
@@ -21,10 +22,8 @@ def create_diag_table_file(**kwargs):
     print_input_args(dictionary)
     import_vars(dictionary=dictionary)
 
-    #import needed environment variables
-    IMPORTS = [ 'VERBOSE', 'DIAG_TABLE_FN', 'DIAG_TABLE_FN', 'DIAG_TABLE_TMPL_F',
-                'CDATE', 'CRES', 'USHDIR']
-    import_vars(env_vars=IMPORTS)
+    #import all environment variables
+    import_vars()
     
     #create a diagnostic table file within the specified run directory
     print_info_msg(f'''
@@ -43,14 +42,14 @@ def create_diag_table_file(**kwargs):
         
         to create:
         
-            diag_table_fp = \"{diag_table_fp}\"''', verboes=VERBOSE)
+            diag_table_fp = \"{diag_table_fp}\"''', verbose=VERBOSE)
 
     settings = dedent(f'''
-            starttime: !datetime {CDATE}
+            starttime: !datetime {type_to_str(CDATE)}
             cres: {CRES}''')
 
     #call fill jinja
-    (err,_,_) = run_command( f'{USHDIR}/fill_jinja_template.py -q -u "{settings}" -t "{DIAG_TABLE_TMPL_FP}" -o "{diag_table_fp}')
+    (err,_,_) = run_command( f'''{USHDIR}/fill_jinja_template.py -q -u "{settings}" -t {DIAG_TABLE_TMPL_FP} -o {diag_table_fp}''')
 
     if err != 0:
         print_err_msg_exit(f'''
@@ -59,11 +58,23 @@ def create_diag_table_file(**kwargs):
             fill_jinja_template.py failed!
             
             !!!!!!!!!!!!!!!!!''')
+        return False
+    return True
 
 class Testing(unittest.TestCase):
     def test_create_diag_table_file(self):
-        self.assertTrue(False)
+        self.assertTrue(\
+                create_diag_table_file( \
+                      run_dir=f"{os.getenv('USHDIR')}/test_data"))
     def setUp(self):
-        define_macro_utilities();
-        set_env_var('DEBUG','FALSE')
+        USHDIR = os.path.dirname(os.path.abspath(__file__))
+        DIAG_TABLE_FN="diag_table"
+        DIAG_TABLE_TMPL_FP = f'{USHDIR}/templates/{DIAG_TABLE_FN}.FV3_GFS_v15p2'
+        set_env_var('DEBUG',True)
+        set_env_var('VERBOSE',True)
+        set_env_var("USHDIR",USHDIR)
+        set_env_var("DIAG_TABLE_FN",DIAG_TABLE_FN)
+        set_env_var("DIAG_TABLE_TMPL_FP",DIAG_TABLE_TMPL_FP)
+        set_env_var("CRES","C48")
+        set_env_var("CDATE","2021010106")
 
