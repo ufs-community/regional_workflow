6a8dd6c8a5daf565b3a753f5fc87125e3beba029
EdwardSnyder-NOAA <96196752+EdwardSnyder-NOAA@users.noreply.github.com>
2022-02-25 14:11:33 -0600

    Create singularity.sh

---
+++ ush/machine/singularity.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+set -x
+
+function file_location() {
+
+  # Return the default location of external model files on disk
+
+  local external_file_fmt external_model location
+
+  external_model=${1}
+  external_file_fmt=${2}
+
+  case ${external_model} in
+
+    "*")
+      print_info_msg"\
+        External model \'${external_model}\' does not have a default
+      location. Please set a user-defined file location."
+      ;;
+
+  esac
+  echo ${location:-}
+
+}
+
+
+EXTRN_MDL_SYSBASEDIR_ICS=${EXTRN_MDL_SYSBASEDIR_ICS:-$(file_location \
+  ${EXTRN_MDL_NAME_ICS} \
+    ${FV3GFS_FILE_FMT_ICS})}
+EXTRN_MDL_SYSBASEDIR_LBCS=${EXTRN_MDL_SYSBASEDIR_LBCS:-$(file_location \
+  ${EXTRN_MDL_NAME_LBCS} \
+  ${FV3GFS_FILE_FMT_ICS})}
+
+# System Installations
+MODULE_INIT_PATH=${MODULE_INIT_PATH:-/apps/lmod/lmod/init/sh}
+
+# Architecture information
+WORKFLOW_MANAGER="rocoto"
+NCORES_PER_NODE=${NCORES_PER_NODE:-40}
+SCHED=${SCHED:-"slurm"}
+PARTITION_DEFAULT=${PARTITION_DEFAULT:-"orion"}
+QUEUE_DEFAULT=${QUEUE_DEFAULT:-"batch"}
+PARTITION_HPSS=${PARTITION_HPSS:-"service"}
+QUEUE_HPSS=${QUEUE_HPSS:-"batch"}
+PARTITION_FCST=${PARTITION_FCST:-"orion"}
+QUEUE_FCST=${QUEUE_FCST:-"batch"}
+
+# UFS SRW App specific paths
+FIXgsm=${FIXgsm:-"/contrib/GST/fix/fix_am"}
+FIXaer=${FIXaer:-"/work/noaa/global/glopara/fix/fix_aer"}
+FIXlut=${FIXlut:-"/work/noaa/global/glopara/fix/fix_lut"}
+TOPO_DIR=${TOPO_DIR:-"/contrib/GST/fix/fix_orog"}
+SFC_CLIMO_INPUT_DIR=${SFC_CLIMO_INPUT_DIR:-"/contrib/GST/fix/fix_sfc_climo"}
+FIXLAM_NCO_BASEDIR=${FIXLAM_NCO_BASEDIR:-"/needs/to/be/specified"}
+
+RUN_CMD_SERIAL="time"
+RUN_CMD_UTILS="mpi"
+RUN_CMD_FCST='mpirun -n ${PE_MEMBER01} --oversubscribe'
+RUN_CMD_POST="mpi"
+
+# Test Data Locations
+TEST_EXTRN_MDL_SOURCE_BASEDIR=/work/noaa/gsd-fv3-dev/gsketefia/UFS/staged_extrn_mdl_files
+
+ulimit -s unlimited
+ulimit -a
