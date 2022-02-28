bf82e6c68020e53a0ae9600128fac0f6692a7271
jeff beck <jeff.beck@noaa.gov>
2022-02-28 22:15:51 +0000

    Update Thompson MP SPP settings.

--- ush/config_defaults.sh
+++ ush/config_defaults.sh
@@ -1827,12 +1827,12 @@ USE_ZMTNBLCK="FALSE"
 #
 DO_SPP="false"
 SPP_VAR_LIST=( "pbl" "sfc" "mp" "rad" "gwd" )
-SPP_MAG_LIST=( "0.2" "0.2" "0.2" "0.2" "0.2" ) #Variable "spp_prt_list" in input.nml
+SPP_MAG_LIST=( "0.2" "0.2" "0.75" "0.2" "0.2" ) #Variable "spp_prt_list" in input.nml
 SPP_LSCALE=( "150000.0" "150000.0" "150000.0" "150000.0" "150000.0" )
 SPP_TSCALE=( "21600.0" "21600.0" "21600.0" "21600.0" "21600.0" ) #Variable "spp_tau" in input.nml
 SPP_SIGTOP1=( "0.1" "0.1" "0.1" "0.1" "0.1")
 SPP_SIGTOP2=( "0.025" "0.025" "0.025" "0.025" "0.025" )
-SPP_STDDEV_CUTOFF=( "1.5" "1.5" "1.5" "1.5" "1.5" )
+SPP_STDDEV_CUTOFF=( "1.5" "1.5" "2.5" "1.5" "1.5" )
 ISEED_SPP=( "4" "4" "4" "4" "4" )
 #
 #-----------------------------------------------------------------------
