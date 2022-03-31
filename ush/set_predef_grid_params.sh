279584521ca0e3b031a4cd980173026c6c871a66
Benjamin.Blake EMC <Benjamin.Blake@v71a1.ncep.noaa.gov>
2022-03-31 18:16:53 +0000

    Increase size of 3-km RRFS CONUS grid

--- ush/set_predef_grid_params.sh
+++ ush/set_predef_grid_params.sh
@@ -201,6 +201,52 @@ case ${PREDEF_GRID_NAME} in
   ESGgrid_DELX="3000.0"
   ESGgrid_DELY="3000.0"
 
+  ESGgrid_NX="1820"
+  ESGgrid_NY="1092"
+
+  ESGgrid_PAZI="0.0"
+
+  ESGgrid_WIDE_HALO_WIDTH="6"
+
+  DT_ATMOS="${DT_ATMOS:-36}"
+
+  LAYOUT_X="${LAYOUT_X:-28}"
+  LAYOUT_Y="${LAYOUT_Y:-28}"
+  BLOCKSIZE="${BLOCKSIZE:-29}"
+
+  if [ "$QUILTING" = "TRUE" ]; then
+    WRTCMP_write_groups="1"
+    WRTCMP_write_tasks_per_group=$(( 1*LAYOUT_Y ))
+    WRTCMP_output_grid="lambert_conformal"
+    WRTCMP_cen_lon="${ESGgrid_LON_CTR}"
+    WRTCMP_cen_lat="${ESGgrid_LAT_CTR}"
+    WRTCMP_stdlat1="${ESGgrid_LAT_CTR}"
+    WRTCMP_stdlat2="${ESGgrid_LAT_CTR}"
+    WRTCMP_nx="1799"
+    WRTCMP_ny="1059"
+    WRTCMP_lon_lwr_left="-122.719258"
+    WRTCMP_lat_lwr_left="21.138123"
+    WRTCMP_dx="${ESGgrid_DELX}"
+    WRTCMP_dy="${ESGgrid_DELY}"
+  fi
+  ;;
+#
+#-----------------------------------------------------------------------
+#
+# The RRFS CONUS domain with ~3km cells that can be initialized from the HRRR.
+#
+#-----------------------------------------------------------------------
+#
+"RRFS_CONUS_3km_HRRRIC")
+
+  GRID_GEN_METHOD="ESGgrid"
+
+  ESGgrid_LON_CTR="-97.5"
+  ESGgrid_LAT_CTR="38.5"
+
+  ESGgrid_DELX="3000.0"
+  ESGgrid_DELY="3000.0"
+
   ESGgrid_NX="1748"
   ESGgrid_NY="1038"
 
