06a188c6cb956811015f284eaa7b9ee6404cc9fa
Yunheng Wang <yunheng.wang@noaa.gov>
2022-04-07 21:33:37 +0000

    Added a fixed WoF grid and the python tool to determine the write component parameters

--- ush/valid_param_vals.sh
+++ ush/valid_param_vals.sh
@@ -27,6 +27,7 @@ valid_vals_PREDEF_GRID_NAME=( \
 "GSD_HRRR_AK_50km" \
 "RRFS_NA_13km" \
 "RRFS_NA_3km" \
+"WoFS_3km" \
 )
 valid_vals_CCPP_PHYS_SUITE=( \
 "FV3_GFS_2017_gfdlmp" \
@@ -37,7 +38,7 @@ valid_vals_CCPP_PHYS_SUITE=( \
 "FV3_RRFS_v1beta" \
 "FV3_RRFS_v1alpha" \
 "FV3_HRRR" \
-) 
+)
 valid_vals_GFDLgrid_RES=("48" "96" "192" "384" "768" "1152" "3072")
 valid_vals_EXTRN_MDL_NAME_ICS=("GSMGFS" "FV3GFS" "RAP" "HRRR" "NAM")
 valid_vals_EXTRN_MDL_NAME_LBCS=("GSMGFS" "FV3GFS" "RAP" "HRRR" "NAM")
