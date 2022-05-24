7267e5acd18d02c5f578f64ec76ad6bd16350c21
Daniel Abdi <daniel.abdi@noaa.gov>
2022-05-24 04:18:16 +0000

    Reorganize source_config.

--- ush/bash_utils/source_config.sh
+++ ush/bash_utils/source_config.sh
@@ -5,7 +5,7 @@
 #-----------------------------------------------------------------------
 #
 
-function config_to_shell_str() {
+function config_to_str() {
 #
 #-----------------------------------------------------------------------
 #
@@ -36,11 +36,7 @@ function config_to_shell_str() {
 #
   local ushdir=${scrfunc_dir%/*}
 
-  if [ $# -eq 1 ]; then
-    python3 $ushdir/config_utils.py -c $1 -o shell -f
-  else
-    python3 $ushdir/config_utils.py -c $1 -o shell -f --keys "${@: 2}"
-  fi
+  $ushdir/config_utils.py -o $1 -c $2 -t $ushdir/config_defaults.yaml "${@: 3}"
 
 #
 #-----------------------------------------------------------------------
@@ -54,53 +50,22 @@ function config_to_shell_str() {
 
 }
 
+#
+#-----------------------------------------------------------------------
+# Define functions for different file formats
+#-----------------------------------------------------------------------
+#
+function config_to_shell_str() {
+    config_to_str shell "$@"
+}
+function config_to_ini_str() {
+    config_to_str ini "$@"
+}
 function config_to_yaml_str() {
-#
-#-----------------------------------------------------------------------
-#
-# Save current shell options (in a global array).  Then set new options
-# for this script/function.
-#
-#-----------------------------------------------------------------------
-#
-  { save_shell_opts; set -u +x; } > /dev/null 2>&1
-
-#
-#-----------------------------------------------------------------------
-#
-# Get the full path to the file in which this script/function is located 
-# (scrfunc_fp), the name of that file (scrfunc_fn), and the directory in
-# which the file is located (scrfunc_dir).
-#
-#-----------------------------------------------------------------------
-#
-  local scrfunc_fp=$( $READLINK -f "${BASH_SOURCE[0]}" )
-  local scrfunc_fn=$( basename "${scrfunc_fp}" )
-  local scrfunc_dir=$( dirname "${scrfunc_fp}" )
-
-#
-#-----------------------------------------------------------------------
-# Get the contents of a config file as yaml string
-#-----------------------------------------------------------------------
-#
-  local ushdir=${scrfunc_dir%/*}
-
-  if [ $# -eq 1 ]; then
-    python3 $ushdir/config_utils.py -c $1 -o yaml -t $ushdir/config_defaults.yaml
-  else
-    python3 $ushdir/config_utils.py -c $1 -o yaml -t $ushdir/config_defaults.yaml --keys "${@: 2}"
-  fi
-
-#
-#-----------------------------------------------------------------------
-#
-# Restore the shell options saved at the beginning of this script/func-
-# tion.
-#
-#-----------------------------------------------------------------------
-#
-  { restore_shell_opts; } > /dev/null 2>&1
-
+    config_to_str yaml "$@"
+}
+function config_to_json_str() {
+    config_to_str json "$@"
 }
 
 #
